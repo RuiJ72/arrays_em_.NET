764393db785a3d6ae477d901b6d06df98ef75b16
RuiJ72 <rlagos900@gmail.com>
2022-04-04 17:30:25 -0300

    Debugando o código para verificar o comportamento do código

--- Colecoes/helper/OperacoesArray.cs
+++ Colecoes/helper/OperacoesArray.cs
@@ -23,12 +23,24 @@ namespace Colecoes.helper
                 }
             }
         }
+        
+        
+        
+        
         public void ImprimirArray(int[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
+        {   
+            
+            
             {
-                System.Console.WriteLine(array[i]);
+                var linha = string.Join(",", array);
+                System.Console.WriteLine(linha);
             }
+        
         }
+    
     }
 }
+// Concatenando os elementos em uma mesma linha 
+
+// Colocar um BreakPoint na linha 11 e observar a lógica do código dando F5
+// Após Colocar o BraekPoint na linha 25 3 debugar. Observar o comportamnto
